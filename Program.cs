709526efbf9740674e09d64755b656f6aa51e3f0
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-11-05 21:36:15 +0600

    Hw15

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _15._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new List<string>
+            {
+                "Ламборгини", "ВАЗ", "Нисан","Таета","Хонда","Ферари","Порш"
+            };
+
+            var result = List.AsParallel()
+                .Where(carName =>
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - {carName} ");
+                    return carName.Contains("a");
+                })
+        .ToList();
+            Console.ReadLine();
+        }
+    }
+}
